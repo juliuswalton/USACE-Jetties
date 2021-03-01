30215fb7ba52df8349f342102939c3fd00fd9ddf
garrisonjd <garrisonjd@g.cofc.edu>
2021-03-01 17:53:17 -0500

    added division layer and toggable basemap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useRef, useEffect } from "react";
 import ArcGISMap from "@arcgis/core/Map";
 //import DictionaryRenderer from "@arcgis/core/renderers/DictionaryRenderer";
 import MapView from "@arcgis/core/views/MapView";
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import LayerList from "@arcgis/core/widgets/LayerList";
+import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
 import esriConfig from '@arcgis/core/config.js';
 
 import "./App.css";
@@ -10,7 +13,7 @@ import "./App.css";
 function App() {
 
   // Required: Set this property to insure assets resolve correctly.
-  esriConfig.assetsPath = './assets'; 
+  esriConfig.assetsPath = './assets';
 
   const mapDiv = useRef(null);
 
@@ -19,12 +22,27 @@ function App() {
       /**
        * Initialize application
        */
+
+      const template = {
+        title: "USACE Civil Works Division",
+        content: "{DIVISION} {DIV_SYM}",
+      };
+
+      const geoLayer = new GeoJSONLayer({
+        url: "https://opendata.arcgis.com/datasets/4cdab8820d7a4f58aaa003be63f059ac_0.geojson",
+        copyright: "USACE Civil Works",
+        popupTemplate: template,
+        title: "USACE Divisions",
+        listMode: false
+      });
+
       const map = new ArcGISMap({
-        basemap: "gray-vector",
+        basemap: "topo-vector",
+        layers: [geoLayer]
       });
 
       const view = new MapView({
-        map,
+        map: map,
         container: mapDiv.current,
         extent: {
           spatialReference: {
@@ -38,15 +56,26 @@ function App() {
         zoom: 4
       });
 
-      view.on("click", function(e){
-        console.log(view.zoom);
-        console.log(view.extent.toJSON());
+      view.when(function () {
+        var layerList = new LayerList({
+          view: view,
+
+        });
+
+        view.ui.add(layerList, "top-right");
       });
-     
+      var toggle = new BasemapToggle({
+        // 2 - Set properties
+        view: view, // view that provides access to the map's 'topo-vector' basemap
+        nextBasemap: "hybrid" // allows for toggling to the 'hybrid' basemap
+      });
+      view.ui.add(toggle, "top-right");
     }
   }, []);
 
-  return <div className="mapDiv" ref={mapDiv}></div>;
+  return (<div className="mapDiv" ref={mapDiv}></div>
+
+  );
 }
 
 export default App;
