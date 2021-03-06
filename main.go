dab8e50914139229e32555d499e943fd0e8915bc
Julius Walton <nami@juliuss-mbp.home>
2021-03-06 03:17:34 -0500

    connected go to the postgresql database

--- main.go
+++ main.go
@@ -1,29 +1,69 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+
+	//_ "github.com/lib/pq"
+	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
 )
 
-/*func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-	}
-	fmt.Fprint(w, "Hello, World!")
-}*/
+const (
+	instanceConnection = "strange-tome-305601:us-east1:vessel-data"
+	databaseName       = "postgres"
+	user               = "postgres"
+	password           = "capstone"
+)
 
 func main() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", instanceConnection, user, password, databaseName)
+	db, err := sql.Open("cloudsqlpostgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
 
+	rows, err := db.Query("SELECT * FROM vessels")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var (
+		vessel_id          string
+		vessel_description string
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&vessel_id, &vessel_description)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("%s: %s", vessel_id, vessel_description)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
 
